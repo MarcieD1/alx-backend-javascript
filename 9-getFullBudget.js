a740be252eb819e8c0426e4a90f32ef0fe1aa85a
MarcieD1 <marciatarisai@icloud.com>
2024-05-27 20:59:00 +0200

    Get the full Budget

---
+++ 9-getFullBudget.js
@@ -0,0 +1,12 @@
+import getBudgetoBJECT FROM './7-GetBudgetObject';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+	const budget = getBudgetObject(income, gdp, capita);
+	const fullBudget = {
+	  ...budget,
+	  getIncomeInDollars:(income) => '$${income}',
+	  getIncomeInEuros:(income) => '${income} euros',
+	};
+
+	return fullBudget;
+       }
