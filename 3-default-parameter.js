ea841d05ecb2d6a9e71d551d0fe492949772c844
MarcieD1 <marciatarisai@icloud.com>
2024-05-28 19:48:59 +0200

    moved

--- 3-default-parameter.js
+++
@@ -1,3 +0,0 @@
-export default function getSumOfHoods(initialNumber, expanion1989 = 89, expansion2019 = 19) {
-  return initialNumber + expansion1989 + expansion 2019;
-}
--- README.md
+++
@@ -1,4 +0,0 @@
-0x00-ES6 basic
-ES6 Basics 
-Understanding what ES6 Basics is.
-The difference between constant and a variable.
