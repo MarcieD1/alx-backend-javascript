c2fd25a2e0b26166056f3f10f13025c7bb374e23
MarcieD1 <marciatarisai@icloud.com>
2024-07-25 19:23:11 +0200

    Stubs

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,15 @@
+const sinon = require('sinon');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./4-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  it('should call Utils.calculateNumber with SUM, 100, and 20 and log the correct message', () => {
+    const stub = sinon.stub(Utils, 'calculateNumber').returns(10);
+    const spy = sinon.spy(console, 'log');
+    sendPaymentRequestToApi(100, 20);
+    sinon.assert.calledWith(stub, 'SUM', 100, 20);
+    sinon.assert.calledWith(spy, 'The total is: 10');
+    stub.restore();
+    spy.restore();
+  });
+});
