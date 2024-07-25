f7ac01ec8374fb0568e89251c1eb339f8fd0f8e3
MarcieD1 <marciatarisai@icloud.com>
2024-07-25 17:07:30 +0200

    wq

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-   from typing import Callable
-
-   def make_multiplier(multiplier: float) -> Callable[[float], float]:
-       def multiplier_func(x: float) -> float:
-           return x * multiplier
-       return multiplier_func
